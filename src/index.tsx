891bd992dd88b2f1e09d166a94d9859ae62ff2df
pocari <caffelattenonsugar@gmail.com>
2020-02-23 17:16:52 +0900

    三目並べ完成

--- src/index.tsx
+++ src/index.tsx
@@ -36,8 +36,11 @@ class Board extends React.Component<{}, BoardState> {
     }
   }
 
-  handleClick(i: number): void {
+  private handleClick(i: number): void {
     const squares = this.state.squares.slice()
+    if (this.calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
@@ -45,6 +48,25 @@ class Board extends React.Component<{}, BoardState> {
     })
   }
 
+  private calculateWinner(squares: SquareValue[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+    return null;
+  }
   private renderSquare(i: number) {
     return (
       <Square
@@ -56,7 +78,13 @@ class Board extends React.Component<{}, BoardState> {
 
 
   public render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    const winner = this.calculateWinner(this.state.squares)
+    let status: string
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
 
     return (
       <div>
